0d38932d322e559d5957e67c3346e1462b142cac
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-05-30 16:26:17 +0800

    Update output message

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ function Welcome(props) {
   return <h1>{props.message}</h1>;
 }
 
-const element = <Welcome message="👋 🌏 Hello, world in React with JSX!" />;
+const element = (
+  <Welcome message="👋 🌏 Hello, world in React with function components!" />
+);
 
 const container = document.getElementById("app");
 
